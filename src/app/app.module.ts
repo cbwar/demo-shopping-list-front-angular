16c3253b9c3de5847d33240bd19ee2520ecac5eb
Raphael Lisch <cbwar@protonmail.com>
2021-12-25 13:47:56 +0100

    add main layout base components, add bootstrap 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListsComponent } from './components/shopping-lists/shopping-lists.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingListComponent,
+    ShoppingListsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
